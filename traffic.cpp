ab61f8d3d25d2dc199a25e6481824d13bb321da2
Meshack <meshackmutinda555@gmail.com>
2023-07-30 14:35:12 +0300

    Update traffic.cpp

--- traffic.cpp
+++ traffic.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 
-// Function that predict traffic congestion level
+// Function that predicts traffic congestion level
 std::string predictCongestion(double congestionIndex) {
     if (congestionIndex < 0.3) {
         return "Low congestion";
@@ -17,23 +16,31 @@ std::string predictCongestion(double congestionIndex) {
 }
 
 int main() {
-    //Seed random number generator
-    std::srand(static_cast<unsigned>(std::time(0)));
+    // Seed random number generator with current time
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(0.0, 1.0);
 
-    //Simulate historical traffic data (congestion index between 0.0 and 1.0)
+    // Simulate historical traffic data (congestion index between 0.0 and 1.0)
     std::vector<double> historicalData = { 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.4, 0.3, 0.9, 1.0 };
 
-    //Simulate real-time sensor data (congestion index between 0.0 and 1.0)
-    double realTimeData = static_cast<double>(std::rand()) / RAND_MAX;
+    // Check if historicalData is empty
+    if (historicalData.empty()) {
+        std::cerr << "Error: Historical data is empty." << std::endl;
+        return 1;
+    }
 
-    //Train the model
+    // Simulate real-time sensor data (congestion index between 0.0 and 1.0)
+    double realTimeData = dis(gen);
+
+    // Train the model
     double sum = 0.0;
     for (double data : historicalData) {
         sum += data;
     }
     double averageCongestion = sum / historicalData.size();
 
-    //Prediction calculation
+    // Prediction calculation
     double prediction = (averageCongestion + realTimeData) / 2.0;
     std::string congestionLevel = predictCongestion(prediction);
 
